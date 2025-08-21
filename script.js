61e608806da02b9de744b4443d0a72f9f9c68871
olivares-pim <69550228+Emil-OP@users.noreply.github.com>
2025-08-20 23:30:30 -0400

    Add client registration form with modal functionality
    Introduced a new client registration form in index.html, styled and positioned as a modal in style.css. Updated script.js to handle opening and closing the form and improved overlay and search modal interactions for better user experience.

--- script.js
+++ script.js
@@ -23,6 +23,21 @@
             // Add button functionality
 
              const floatingAdd = document.getElementById("floatingAdd");
+             const formContainer = document.getElementById("formContainer");
+
+             function openForm(){
+                formContainer.classList.add('open');
+                focusOverlay.classList.add('active');
+             }
+
+             function closeForm(){
+                formContainer.classList.remove('open');
+                focusOverlay.classList.remove('active');
+            }
+
+                floatingAdd.addEventListener('click', openForm);
+                focusOverlay.addEventListener('click', closeForm);
+               
 
             // Search button functionality
             
@@ -32,14 +47,22 @@
             function openSearch(){
                 floatingSearch.classList.add('open');
                 floatingSearchInput.classList.add('active')
+                focusOverlay.classList.add('active');
                 floatingSearchInput.focus();
             }
 
             function closeSearch(){
                 floatingSearch.classList.remove('open');
                 floatingSearchInput.classList.remove('active');
+                focusOverlay.classList.remove('active');
             }
 
             floatingSearch.addEventListener('click', openSearch);
             floatingSearchInput.addEventListener('blur', closeSearch);
+            focusOverlay.addEventListener('click', closeSearch);
+            floatingSearchInput.addEventListener('keydown', function(event) {
+                if (event.key === 'Enter' || event.key === 'Escape') {
+                    closeSearch();
+                }
+            });
 
